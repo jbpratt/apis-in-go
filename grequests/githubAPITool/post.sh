b1d1aa8e9a62e4c9015ee88599febcbafe4a8138
jbpratt78 <jbpratt78@gmail.com>
2019-03-11 05:19:58 -0500

    removed revoked token

--- grequests/githubAPITool/post.sh
+++ grequests/githubAPITool/post.sh
@@ -2,5 +2,5 @@ curl -X POST \
   http://api.github.com/gists \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
-  -H 'authToken: ec765b468e647af09c2e08033d022044942a6926' \
+  -H 'authToken: xxxxxxxxxxxxxxxxxxxxxxxxxxx' \
   -d ./test.txt
